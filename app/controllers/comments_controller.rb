b33455f5c1826cd9ecfe040cdb74b6f8f14b8777
C-56033 <c-56033@134-68-178-147.dhcp-in.iupui.edu>
2015-07-30 15:10:09 -0400

    Ignore files not needed for deployment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -25,14 +25,14 @@ class CommentsController < ApplicationController
   # POST /comments.json
   def create
     @comment = Comment.new(comment_params)
+    @transcription = @comment.transcription
 
-    respond_to do |format|
+  respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
-        format.json { render :show, status: :created, location: @comment }
+      format.html { redirect_to transcription_path(@transcription), notice: 'Comment was successfully created.' }
       else
-        format.html { render :new }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      format.html { render :new }
+      format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
   end
